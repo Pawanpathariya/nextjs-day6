b3b26f6302cbd6d43538dbf5a671d3409c8e5242
Pawanpathariya <pawanpathariys@gmail.com>
2025-04-16 18:54:30 +0530

    k

--- app/pages/register/page.jsx
+++ app/pages/register/page.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 import schema from '@/app/middleware/zodschema'
+import { useTheme } from 'next-themes'
 const page = () => {
     const [data, setData] = useState({});
+    const { theme, setTheme } = useTheme();
 
     const handleInput = (e) => {
         setData({ ...data, [e.target.name]: e.target.value });
@@ -34,34 +36,34 @@ const page = () => {
   return (
   <>
   
-  <div className="flex items-center justify-center min-h-screen bg-gray-100">
-    <div className="px-8 py-6 mx-4 mt-4 text-left bg-white shadow-lg md:w-1/2 lg:w-1/3 sm:w-1/2">
+  <div className="flex items-center justify-center min-h-screen" style={{backgroundColor: theme === 'dark' ? '#1A202C' : '#fff'}}>
+    <div className="px-8 py-6 mx-4 mt-4 text-left" style={{backgroundColor: theme === 'dark' ? '#1A202C' : '#fff'}}>
       <div className="flex justify-center">
         <svg xmlns="http://www.w3.org/2000/svg" className="w-20 h-20 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
         </svg>
       </div>
-      <h3 className="text-3xl font-bold text-center">Register</h3>
+      <h3 className="text-3xl font-bold text-center" style={{color: theme === 'dark' ? '#fff' : '#000'}}>Register</h3>
       <form>
         <div className="mt-4">
           <div>
             <label className="block" htmlFor="name">Name</label>
-            <input type="text" id="name" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" name='name' onChange={handleInput} />
+            <input type="text" id="name" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" name='name' onChange={handleInput} style={{backgroundColor: theme === 'dark' ? '#2D3748' : '#fff', color: theme === 'dark' ? '#fff' : '#000'}} />
           </div>
           <div className="mt-4">
             <label className="block" htmlFor="email">Email</label>
-            <input type="email" id="email" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" name='email' onChange={handleInput} />
+            <input type="email" id="email" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" name='email' onChange={handleInput} style={{backgroundColor: theme === 'dark' ? '#2D3748' : '#fff', color: theme === 'dark' ? '#fff' : '#000'}} />
           </div>
           <div className="mt-4">
             <label className="block" htmlFor="email">Phone</label>
-            <input type="text" id="phone" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" name='phone' onChange={handleInput} />
+            <input type="text" id="phone" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" name='phone' onChange={handleInput} style={{backgroundColor: theme === 'dark' ? '#2D3748' : '#fff', color: theme === 'dark' ? '#fff' : '#000'}} />
           </div>
           <div className="mt-4">
             <label className="block" htmlFor="password">Password</label>
-            <input type="password" id="password" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" name='password'onChange={handleInput}  />
+            <input type="password" id="password" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" name='password'onChange={handleInput} style={{backgroundColor: theme === 'dark' ? '#2D3748' : '#fff', color: theme === 'dark' ? '#fff' : '#000'}}  />
           </div>
           <div className="flex items-baseline justify-between">
-            <button className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900" onClick={handleSubmit}>Register</button>
+            <button className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900" onClick={handleSubmit} style={{backgroundColor: theme === 'dark' ? '#2D3748' : '#fff', color: theme === 'dark' ? '#fff' : '#000'}}>Register</button>
           </div>
         </div>
       </form>
@@ -72,3 +74,4 @@ const page = () => {
 }
 
 export default page
+
